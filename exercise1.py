13117319a159cf690e869eebf76d865df3403fef
randomguy069 <32550783+randomguy069@users.noreply.github.com>
2017-12-21 23:38:34 +0530

    Add files via upload

--- exercise1.py
+++ exercise1.py
@@ -1,7 +1,12 @@
-fi = open("fruits.txt",'r')
-
-content = fi.readlines()
-con =[i.rstrip("\n") for i in content]
-for i in con:
-    print (i)
-fi.close()
+import datetime
+filename = datetime.datetime.now()
+fil1 = open(filename.strftime("%Y-%m-%d-%H-%M-%S")+".txt","w+")
+fil2 = open("file1.txt",'r');
+fil3 = open("file2.txt",'r');
+fil4 = open("file3.txt",'r');
+con2 = fil2.read()
+con3 = fil3.read()
+con4 = fil4.read()
+fil1.write(con2+"\n")
+fil1.write(con3+"\n")
+fil1.write(con4+"\n")
